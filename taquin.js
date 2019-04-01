5da851c03b918adddb84a03dc1ca8fb50380d613
Isnard <enzoisnard@gmail.com>
2019-04-01 16:53:46 +0200

    Changement dans taquini.js (toujours bugué)

--- taquin.js
+++ taquin.js
@@ -1,30 +1,74 @@
 window.onload = function () {
-
-	// pour distinguer les premiers clics
-	// des seconds clics
-	let first_click = true;
-
-	// pour stocker la première image cliquée
-	let first_image;
+	
+	blanc = document.getElementsByName("9");
 
 	// si "not_finished" est vrai, alors
 	// il reste des images à permuter
 	let not_finished = true;
 
-	// traîte le clic sur une image
-	function click_on() {
-		if (first_click) {
-			first_click = false;
-			first_image = this;
-
+	//Compteur de coups
+	let compteur = 0;
+	function nb_permutations(array){
+		let out = array;
+		let i = 2;
+		let n = 0;
+		while(i<array.length-1){
+			let j = 0
+			while(j<i-1){
+				if(out[j+1]<out[j]){
+					let temp = out[j];
+					out[j] = out[j+1];
+					out[j+1] = temp;
+					n++;
+				}
+				j++
+			}
+			i++;
 		}
-		else {
-			swap(first_image, this);
-			first_click = true;
-			is_finished();
-		}
+		return n;
 
 	}
+
+	function isSolvable(array){
+		indice_blanc = array.indexOf(blanc);
+		distance = 9-indice_blanc;
+		return nb_permutations(array)%2 == distance%2;
+	}
+
+
+	// traîte le clic sur une image
+	function click_on() {
+		blanc = document.getElementsByName("9")[0];
+	
+		array = Array.from(document.getElementsByTagName("img"));
+		
+		
+		indice_blanc = array.indexOf(blanc);
+		indice_this = array.indexOf(this);
+		
+		
+		
+		
+		if(isMovable(indice_blanc,indice_this)){
+			
+			swap(this,blanc);
+			is_finished();
+		}
+	}
+
+
+	function isMovable(indice_blanc,indice_this){
+		if (indice_blanc%3==1){
+			return (indice_blanc == indice_this+3 || indice_blanc == indice_this-3 || indice_blanc==indice_this-1);
+		}
+		else if (indice_blanc%3==0){
+			return (indice_blanc == indice_this+3 || indice_blanc == indice_this-3 || indice_blanc==indice_this+1);
+		}
+		else{
+			return (indice_blanc == indice_this+3 || indice_blanc == indice_this-3 || indice_blanc==indice_this+1 || indice_blanc==indice_this-1);
+		}
+	}
+
 
 	function isSort(array) {
 		for (let i = 0; i < array.length - 1; i++) {
@@ -34,25 +78,21 @@ window.onload = function () {
 	}
 
 	// teste si le puzzle est terminé
-	function is_finished() {
+	function is_finished(){
 		let out = [];
         let array = document.getElementsByTagName("img");
         
 		for (let i = 1; i < array.length; i++) {
 			out.push(array[i].name);
-
 		}
+		console.log(isSolvable(out));
 
 		if (isSort(out)) {
 			let result = document.getElementById("result");
+			result.innerHTML = "Bravo vous avez fini en " + compteur + "coups GG.";
 			result.style.visibility = "visible";
 
 		}
-
-		
-
-
-
 
 	}
 
@@ -63,14 +103,11 @@ window.onload = function () {
 		img1.name = img2.name;
 		img2.src = temp_src;
 		img2.name = temp_name;
+		compteur +=1;
 	}
-
 
 	let array = document.getElementsByTagName("img");
 	for (let i = 1; i < array.length; i++) {
 		array[i].onclick = click_on;
 	}
-	// ici, il faut relier la fonction "clic_on" à l'évènement "onclick"
-	// sur toutes les images contenues dans l'élément d'id "puzzle"
-
 };
